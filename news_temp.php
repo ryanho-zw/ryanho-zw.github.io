cc4f0255fbb8d712c6e35ed54fb721624c77e4ee
TacoKirin <46987899+TacoKirin@users.noreply.github.com>
2021-07-16 17:06:39 +0800

    php test

--- news_temp.php
+++ news_temp.php
@@ -4,7 +4,7 @@ date_default_timezone_set("Asia/Taipei");
 $date = date("ymdHi", time());
 echo (int)$date;
 
-if ( (int)$date > 21080821 ) {
+if ( (int)$date > 2108082100 ) {
 	echo "Off";
 }
 else {
